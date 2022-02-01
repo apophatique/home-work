0f8d3d55ddbac4d9183d9ed44e729859cb34de9f
gsattarov <gsattarov@lanit.ru>
2022-02-01 18:22:54 +0600

    homework-07 тест обновлен

--- src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
+++ src/test/java/com/sbrf/reboot/equalshashcode/EqualsHashCodeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Objects;
 
 public class EqualsHashCodeTest {
 
@@ -21,10 +22,25 @@ public class EqualsHashCodeTest {
             if (o == this)
                 return true;
 
-            return false;
+            // Сравнение с null
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            // Выше проверили, что это объект класса Car, поэтому для сравнения полей приведем к Car
+            final Car oCar = (Car) o;
+
+            // Сравнение по полям
+            return Objects.equals(model, oCar.model) &&
+                    Objects.equals(color, oCar.color) &&
+                    Objects.equals(releaseDate, oCar.releaseDate) &&
+                    Objects.equals(maxSpeed, oCar.maxSpeed);
+
         }
 
-
+         @Override
+         public int hashCode() {
+             return Objects.hash(model, color, releaseDate, maxSpeed);
+         }
      }
 
     @Test
